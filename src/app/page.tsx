7775eacfd9c61a5d46b3068e9777a2fee32a3d4a
Rafael_Tomazini <rafaelnt83@gmail.com>
2025-03-13 20:33:01 -0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-full items-center justify-center">
+      <h1 className="text-center font-bold text-3xl mt-20">Hello World</h1>
+    </div>
+  );
+}
