91017df54f6bbe6f393317e702af2ded16129a1f
Rafael_Tomazini <rafaelnt83@gmail.com>
2025-03-13 21:24:13 -0300

    pages setup + tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full h-full items-center justify-center">
-      <h1 className="text-center font-bold text-3xl mt-20">Hello World</h1>
+    <div className="w-full h-full items-center justify-center ">
+      <h1 className="">Home page</h1>
     </div>
   );
 }
